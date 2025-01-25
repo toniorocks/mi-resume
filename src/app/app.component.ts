e3809648d6fe1697fac1b8c8b72ff2ef4b0ddb28
toniorocks <marco.izag@gmail.com>
2025-01-25 11:44:53 -0600

    Fix OnInit method on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopMenuButtonComponent } from './top-menu-button/top-menu-button.component';
 import { MiNotificationModule } from './mi-notification/mi-notification.module';
@@ -9,13 +9,17 @@ import { MiModalComponent } from "./mi-modal/mi-modal.component";
   standalone: true,
   imports: [RouterOutlet, TopMenuButtonComponent, MiNotificationModule, MiModalComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mi-resume';
+  state: boolean = true;
 
   constructor(
   ) {}
+  ngOnInit(): void {
+    //throw new Error('Method not implemented.');
+  }
 
   clickedItem(item: string): void {
     if (item === "Edit Timeline") {
