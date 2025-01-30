511b1113b59132719204c455d7647e9ca4f16c4b
toniorocks <marco.izag@gmail.com>
2025-01-29 20:31:23 -0600

    Main components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { TimelineComponent } from './timeline/timeline.component';
+import { MainComponent } from './main/main.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { SkillsComponent } from './skills/skills.component';
 
 export const routes: Routes = [
-  { path: '', component: TimelineComponent },
+  { path: '', component: MainComponent },
+  { path: 'experience', component: TimelineComponent },
+  { path: 'certifications', component: CertificationsComponent },
+  { path: 'skills', component: SkillsComponent },
 ];
